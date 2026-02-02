4febe35dd3baed15bc92ebb7e2a51398c89650b5
Aziz Malek <aziz@telnyx.com>
2026-02-02 23:04:14 +0000

    Remove accelerate extension

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,11 @@
 import { PrismaClient } from "@prisma/client";
-import { withAccelerate } from "@prisma/extension-accelerate";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: ReturnType<typeof createPrismaClient> | undefined;
+  prisma: PrismaClient | undefined;
 };
 
 function createPrismaClient() {
-  // In Prisma 7, we need to pass the datasource URL or use Accelerate
+  // In Prisma 7, we need to pass the datasource URL
   const databaseUrl = process.env.POSTGRES_PRISMA_URL || process.env.DATABASE_URL;
   
   return new PrismaClient({
